a4144e8db1dbce2fe0da5aa5af0d267dbe5c7f6b
KT147 <kevin.tammm@gmail.com>
2025-04-23 14:58:40 +0300

    .

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.sinuette.skooridapp',
   appName: 'skoorid-app2',
   webDir: 'dist',
   plugins: {
